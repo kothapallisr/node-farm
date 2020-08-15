7f3b2680a7d490abb1b449838978a3ad2b22ae4e
Sridhar Kothapalli <sridr8@gmail.com>
2020-08-15 07:52:39 +0530

    added chalk and validator modules

---
+++ notes-app/app.js
@@ -0,0 +1,11 @@
+const validator = require("validator");
+const chalk = require("chalk");
+
+console.log(
+  chalk.bold.inverse.blue("Sri Satchidananda Sadguru Sainath Maharaj Ki Jai!")
+);
+console.log(
+  chalk.bold.inverse.green("Sadguru Sri Sainathuni Sarath Babuji Ki Jai!!")
+);
+
+console.log("nodemon running...");
