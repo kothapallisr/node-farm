0e083567bd91522f7fc5f89e569185e40517fbdb
Sridhar Kothapalli <sridr8@gmail.com>
2020-08-16 18:26:14 +0530

    yargs package added to parse messages

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,35 @@
 const validator = require("validator");
 const chalk = require("chalk");
+const yargs = require("yargs");
 const add = require("./utils.js");
 
-console.log(
-  chalk.bold.inverse.blue("Sri Satchidananda Sadguru Sainath Maharaj Ki Jai!")
-);
-console.log(
-  chalk.bold.inverse.green("Sadguru Sri Sainathuni Sarath Babuji Ki Jai!!")
-);
+// console.log(
+//   chalk.bold.inverse.blue("Sri Satchidananda Sadguru Sainath Maharaj Ki Jai!")
+// );
+// console.log(
+//   chalk.bold.inverse.green("Sadguru Sri Sainathuni Sarath Babuji Ki Jai!!")
+// );
 
-console.log("nodemon running...");
-console.log(add(4, 5));
+yargs.version("1.1.0");
+console.log(yargs.argv);
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
+  },
+});
