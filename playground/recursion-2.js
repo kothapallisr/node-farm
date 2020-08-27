d1be490127e33a45ed35e330dd6015e7d2d98bb2
Sridhar Kothapalli <sridr8@gmail.com>
2020-08-27 10:46:09 +0530

    recursion

--- playground/recursion-2.js
+++ playground/recursion-2.js
@@ -1,1 +1,8 @@
-function sumOfDigits(num) {}
+function sumOfDigits(num) {
+  if (num === 0) {
+    return 0;
+  }
+  return (num % 10) + sumOfDigits(Math.floor(num / 10));
+}
+
+console.log(sumOfDigits(1324234234));
