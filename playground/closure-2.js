7335702173cad87e6e8564d3eaa11fcd3264565f
sridr8 <saisuhas@saimail.com>
2020-08-27 14:48:27 +0530

    closure

--- playground/closure-2.js
+++ playground/closure-2.js
@@ -6,6 +6,8 @@ function makeAdder(x) {
 
 var add5 = makeAdder(5);
 var add10 = makeAdder(10);
+var add15 = makeAdder(15);
 
 console.log(add5(2));  // 7
 console.log(add10(2)); // 12
+console.log(add15(4));
